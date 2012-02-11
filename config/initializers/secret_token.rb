aa23bba195a97cd47a7aeb69075dbfadf090b906
Krzysztof Zalewski <zlw.zalewski@gmail.com>
2012-02-11 11:20:35 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrimaRewal::Application.config.secret_token = 'bf12d314c64f210c339c5b38b8f3a69abfcecb5d5cde09426de3dd31a8f127a0367d992beaad8ab68107cb12479def84b63deef14649dcfe259f6a7387a9fdaa'
