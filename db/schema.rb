b8c0b1fee452384d8e297d3cf1b3a3bdc83fc7b7
Krzysztof Zalewski <zlw.zalewski@gmail.com>
2012-02-12 22:52:29 +0100

    install activeadmin, configure admin routes, apply schema
    remove Routes::DeviseControllers (unused) and specs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,39 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120211115402) do
+ActiveRecord::Schema.define(:version => 20120212173433) do
+
+  create_table "active_admin_comments", :force => true do |t|
+    t.integer  "resource_id",   :null => false
+    t.string   "resource_type", :null => false
+    t.integer  "author_id"
+    t.string   "author_type"
+    t.text     "body"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+    t.string   "namespace"
+  end
+
+  add_index "active_admin_comments", ["author_type", "author_id"], :name => "index_active_admin_comments_on_author_type_and_author_id"
+  add_index "active_admin_comments", ["namespace"], :name => "index_active_admin_comments_on_namespace"
+  add_index "active_admin_comments", ["resource_type", "resource_id"], :name => "index_admin_notes_on_resource_type_and_resource_id"
+
+  create_table "flatpage_translations", :force => true do |t|
+    t.integer  "flatpage_id"
+    t.string   "locale"
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  add_index "flatpage_translations", ["flatpage_id"], :name => "index_flatpage_translations_on_flatpage_id"
+  add_index "flatpage_translations", ["title"], :name => "index_flatpage_translations_on_title", :unique => true
+
+  create_table "flatpages", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
