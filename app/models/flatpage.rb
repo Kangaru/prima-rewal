6d7b9cea1242f7d0456a017b0bd1d327d2c9a2da
Krzysztof Zalewski <zlw.zalewski@gmail.com>
2012-02-12 17:17:49 +0100

    flatpage model

--- app/models/flatpage.rb
+++ app/models/flatpage.rb
@@ -1,3 +1,7 @@
 class Flatpage < ActiveRecord::Base
   translates :title, :content
+  accepts_nested_attributes_for :translations
+
+  validates :title, presence: true, length: { minimum: 3, allow_nil: true }
+  validates :content, presence: true, length: { minimum: 10, allow_nil: true }
 end
