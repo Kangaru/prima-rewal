b88c9557fa7803e6cc57eac5384dea24a46d5497
Krzysztof Zalewski <zlw.zalewski@gmail.com>
2012-02-13 22:18:03 +0100

    adjust Flatpage specs for globalize accessors

--- spec/models/flatpage_spec.rb
+++ spec/models/flatpage_spec.rb
@@ -1,13 +1,11 @@
 require 'spec_helper'
 
 describe Flatpage do
-  it { should validate_presence_of :title }
-  it { should ensure_length_of(:title).is_at_least(3) }
+  Language.all.each do |locale, _|
+    it { should validate_presence_of :"title_#{locale}" }
+    it { should ensure_length_of(:"title_#{locale}").is_at_least(3) }
 
-  it { should validate_presence_of :content }
-  it { should ensure_length_of(:content).is_at_least(10) }
-
-  it "should accept nested attributes for translations" do
-    Flatpage.new.should respond_to :translations_attributes=
+    it { should validate_presence_of :"content_#{locale}" }
+    it { should ensure_length_of(:"content_#{locale}").is_at_least(10) }
   end
 end
