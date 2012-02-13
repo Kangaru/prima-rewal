f099384528e11468ee96c4f71d0cd73423a6affb
Krzysztof Zalewski <zlw.zalewski@gmail.com>
2012-02-13 22:17:18 +0100

    create validation for globalized accessors (title_pl etc.)

--- app/models/flatpage.rb
+++ app/models/flatpage.rb
@@ -1,8 +1,9 @@
 class Flatpage < ActiveRecord::Base
   translates :title, :content
-  accepts_nested_attributes_for :translations
   globalize_accessors
 
-  validates :title, presence: true, length: { minimum: 3, allow_nil: true }
-  validates :content, presence: true, length: { minimum: 10, allow_nil: true }
+  ::Language.all.each do |lang, _|
+    validates :"title_#{lang}", presence: true, length: { minimum: 3, allow_blank: true }
+    validates :"content_#{lang}", presence: true, length: { minimum: 10, allow_blank: true }
+  end
 end
