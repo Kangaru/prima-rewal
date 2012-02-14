84a0675c4cf23cac15b5423f5c5eb84e9f382852
Krzysztof Zalewski <zlw.zalewski@gmail.com>
2012-02-14 21:17:17 +0100

    use FlatpagePresenter in admin

--- app/admin/flatpages.rb
+++ app/admin/flatpages.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register Flatpage do
   config.clear_sidebar_sections!
 
   controller do
-    helper :locale
+    helper :locale, :presenter
 
     protected
     def paginate(chain)
@@ -13,15 +13,15 @@ ActiveAdmin.register Flatpage do
 
   index do
     column :title, sortable: false do |flatpage|
-      flatpage.translations.map(&:title) * '/'
+      present(flatpage) {|p| p.titles}
     end
 
-    column :locales do |flatpage|
-      flatpage.translations.map(&:locale).map {|locale| Language.translate_locale locale } * '/'
+    column :locales, sortable: false do |flatpage|
+      present(flatpage) {|p| p.translated_to }
     end
 
     column :created_at do |flatpage|
-      l flatpage.created_at
+      present(flatpage) {|p| p.created_at }
     end
 
     default_actions
@@ -32,7 +32,7 @@ ActiveAdmin.register Flatpage do
   show do |flatpage|
     attributes_table do
       row :title do
-        flatpage.translations.map(&:title) * '/'
+        present(flatpage) {|p| p.titles }
       end
 
       for_locales do |locale, _|
