58da9336dda8c23598af84caed16332a4533ebb9
Krzysztof Zalewski <zlw.zalewski@gmail.com>
2012-02-21 21:36:12 +0100

    create admin Account page

---
+++ app/admin/account.rb
@@ -0,0 +1,24 @@
+ActiveAdmin.register User, as: 'Account' do
+  actions :edit, :update
+
+  menu parent: 'Users'
+
+  controller do
+    def redirect_to_edit
+      redirect_to edit_admin_account_path current_user
+    end
+
+    alias_method :index, :redirect_to_edit
+    alias_method :show,  :redirect_to_edit
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+    end
+
+    f.buttons
+  end
+end
---
+++ spec/requests/admin/account_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe 'Admin::AcccountsController' do
+  let(:user) { Factory :user }
+  before { sign_in user }
+
+  it 'should edit users account and force user to sign in with new credentials' do
+    visit edit_admin_account_path user
+
+    new_email, new_password = 'my@email.com', 'password'
+
+    within 'form#edit_account' do
+      fill_in 'account_email', with: new_email
+      fill_in 'account_password', with: new_password
+      fill_in 'account_password_confirmation', with: new_password
+
+      click_button 'Update User'
+    end
+
+    current_path.should == new_user_session_path
+
+    within 'form#session_new' do
+      fill_in 'user_email', with: new_email
+      fill_in 'user_password', with: new_password
+
+      click_button 'Login'
+    end
+
+    current_path.should == edit_admin_account_path(user)
+  end
+end
