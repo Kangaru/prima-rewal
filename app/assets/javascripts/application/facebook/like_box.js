e6758fe70a49dc7888c5b583f5fbbcd8059dbb53
Krzysztof Zalewski <zlw.zalewski@gmail.com>
2012-02-28 02:18:40 +0100

    js for fb like box

---
+++ app/assets/javascripts/application/facebook/like_box.js
@@ -0,0 +1,7 @@
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/pl_PL/all.js#xfbml=1&appId=133818496681616";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
