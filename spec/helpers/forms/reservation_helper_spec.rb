793408888001a0310be4f062024d7e3a176473a0
Krzysztof Zalewski <zlw.zalewski@gmail.com>
2012-02-28 01:11:38 +0100

    use have_selector instead of own #extract[tag/attr] methods

--- spec/helpers/forms/reservation_helper_spec.rb
+++ spec/helpers/forms/reservation_helper_spec.rb
@@ -4,29 +4,29 @@ describe Forms::ReservationHelper do
   context "#input" do
     it 'should add input-medium class' do
       helper.reservation_form :reservation do |f|
-        input = extract_tag :input, f.input(:title)
-        extract_attr(:class, input).should include 'input-medium'
+        input = f.input :title
+        input.should have_selector 'input.input-medium'
       end
     end
 
     it 'should add rows=3 if input is text' do
       helper.reservation_form :reservation do |f|
-        input = extract_tag :textarea, f.input(:title, as: :text)
-        extract_attr(:rows, input).should == '3'
+        input = f.input :body, as: :text
+        input.should have_selector 'textarea[rows="3"]'
       end
     end
 
     it 'should allow to add other input_html attributes' do
       helper.reservation_form :reservation do |f|
-        input = extract_tag :input, f.input(:title, input_html: {someattr: :somevalue})
-        extract_attr(:someattr, input).should include 'somevalue'
+        input = f.input :title, input_html: { someattr: :somevalue }
+        input.should have_selector 'input[someattr="somevalue"]'
       end
     end
 
     it 'should set input to optional' do
       helper.reservation_form :reservation do |f|
-        input = extract_tag :label, f.input(:title)
-        extract_attr(:class, input).should include 'optional'
+        f.input(:title).should have_selector 'label.optional'
+        f.input(:title, as: :text).should have_selector 'label.optional'
       end
     end
   end
@@ -34,7 +34,8 @@ describe Forms::ReservationHelper do
   context "#button" do
     it 'should add button classes to button' do
       helper.reservation_form :reservation do |f|
-        extract_attr(:class, f.button(:submit)).should include 'btn-large btn-primary btn-info'
+        input = f.button :submit
+        input.should have_selector 'input.btn-large.btn-primary.btn-info'
       end
     end
   end
--- spec/support/helpers/extract.rb
+++
@@ -1,8 +0,0 @@
-def extract_tag(name, string)
-  match = string.match(/(?<#{name}><#{name} [\s\S]+ \/>)/) || string.match(/(?<#{name}><#{name} [\s\S]+><\/#{name}>)/)
-  match[name]
-end
-
-def extract_attr(name, string)
-  string.match(/#{name}=\"(?<#{name}>[a-z0-9 _-]+)\"/)[name]
-end
