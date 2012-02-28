ac5b3d7f446fa2e9e5ab990899be889f0fb288b3
Krzysztof Zalewski <zlw.zalewski@gmail.com>
2012-02-28 01:38:20 +0100

    #show view (used also in index)

---
+++ spec/requests/flatpages_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe 'FlatpagesController' do
+  context "#index" do
+    let!(:flatpage) { Factory :flatpage }
+    before { visit root_path }
+
+    it 'should show flatpage' do
+      within 'article' do
+        page.should have_content flatpage.content
+      end
+    end
+
+  end
+
+  context "#show" do
+    let!(:flatpage) { Factory :flatpage }
+    before { visit flatpage_path flatpage.id }
+
+    it "should show flatpage" do
+      within 'article' do
+        page.should have_content flatpage.content
+      end
+    end
+  end
+end
