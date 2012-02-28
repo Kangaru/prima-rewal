2c76b3ad21b5e7234ee986b47a539d3a2e6a936a
Krzysztof Zalewski <zlw.zalewski@gmail.com>
2012-02-28 23:36:33 +0100

    move like box to sidebar and change some styling

--- app/helpers/facebook_helper.rb
+++ app/helpers/facebook_helper.rb
@@ -3,7 +3,7 @@ require 'ostruct'
 module FacebookHelper
   def facebook_like_box
     options ||= OpenStruct.new({
-      href:   "http://facebook.com/primarewal", width:  840, height: 185, faces: true, stream: false, header: false, color:  '#ffffff'
+      href:   "http://facebook.com/primarewal", width:  250, height: 80, faces: false, stream: false, header: false, color:  '#ffffff'
     })
 
     render partial: 'helpers/facebook/like_box', locals: { options: options }
--- spec/helpers/facebook_helper_spec.rb
+++ spec/helpers/facebook_helper_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 describe FacebookHelper do
   context "#facebook_like_box" do
     it 'should return like box' do
-      #OpenStruct.should_receive(:new).with(href: "http://www.facebook.com/primarewal", width:  840, height: 185, faces: true, stream: false, header: false, color:  '#ffffff')
       box = helper.facebook_like_box
 
-      box.should have_selector 'div.container'
-      box.should have_selector 'div#fb-root'
+      box.should have_selector 'div.box-shadowed #fb-root'
+      box.should have_selector 'div.box-shadowed .fb-like-box'
+
       box.should have_selector 'div.fb-like-box[data-href="http://facebook.com/primarewal"]'
     end
   end
