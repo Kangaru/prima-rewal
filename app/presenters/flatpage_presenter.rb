f60b66cd05f828d2560b4ffc0f37dc022dc6e146
Krzysztof Zalewski <zlw.zalewski@gmail.com>
2012-03-02 00:53:40 +0100

    use Widget class in FlatpagePresenter and move carousel to flatpage (on main page)

--- app/presenters/flatpage_presenter.rb
+++ app/presenters/flatpage_presenter.rb
@@ -8,7 +8,9 @@ class FlatpagePresenter < ActionPresenter::Base
   let(:titles) {|joiner='/'| flatpage.translations.map(&:title) * joiner }
   let(:sortable_titles) { content_tag :span, titles, data: { id: flatpage.id }, class: :sortable_title  }
 
-  let(:content) { markdown flatpage.content }
+  let(:content) do
+    markdown Widget.new(flatpage.content, self).widgetize
+  end
 
   let(:translated_to) {|joiner='/'| flatpage.translations.map(&:locale).map {|locale| Language.translate_locale locale } * joiner }
 
