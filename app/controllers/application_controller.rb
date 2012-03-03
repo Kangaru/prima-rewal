42a765245b8866c3f53a032dc9e9c244e73367db
Krzysztof Zalewski <zlw.zalewski@gmail.com>
2012-03-03 14:03:31 +0100

    remove changing locale from ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
-  before_filter :set_locale
-
-  def set_locale
-    return redirect_to "/#{I18n.default_locale}" unless params[:locale]
-    I18n.locale = params[:locale]
-  end
-
-  def default_url_options(options={})
-    options.merge({ locale: I18n.locale })
-  end
 end
