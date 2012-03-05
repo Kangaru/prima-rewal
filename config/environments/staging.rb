bd5d2e8abe6763f094056262827951e6526fc016
Krzysztof Zalewski <zlw.zalewski@gmail.com>
2012-03-05 12:52:44 +0100

    change staging environment

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -12,7 +12,7 @@ PrimaRewal::Application.configure do
   config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
-  config.assets.compress = false
+  config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
@@ -64,4 +64,16 @@ PrimaRewal::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'primarewal',
+    password:             'primarewal123',
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 end
