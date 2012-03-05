71192cc73dbf02bb11842849daa51cfc1a4c7bb4
Krzysztof Zalewski <zlw.zalewski@gmail.com>
2012-03-05 20:21:30 +0100

    redirect to correct url after language change

--- app/controllers/flatpages_controller.rb
+++ app/controllers/flatpages_controller.rb
@@ -8,5 +8,9 @@ class FlatpagesController < ApplicationController
 
   def show
     @flatpage = Flatpage.find(params[:id])
+
+    unless url_for(@flatpage) == request.url
+      return redirect_to flatpage_path(@flatpage), status: :moved_permanently
+    end
   end
 end
