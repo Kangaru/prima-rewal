16828acc5e342be7988c3a8e531a8e503d784075
Krzysztof Zalewski <zlw.zalewski@gmail.com>
2012-03-06 20:07:53 +0100

    clean up facebook_like_box cell

--- app/cells/facebook_like_box_cell.rb
+++ app/cells/facebook_like_box_cell.rb
@@ -1,11 +1,15 @@
-require 'ostruct'
-
 class FacebookLikeBoxCell < Cell::Rails
 
-  def display(*args)
-    @options = OpenStruct.new({
-      href:   "http://facebook.com/primarewal", width:  240, height: 80, faces: false, stream: false, header: false, color:  '#ffffff'
-    }.merge args.extract_options!)
+  cache :display, expires_in: 1.hour
+
+  def display
+    @options = {
+      href: "http://facebook.com/primarewal",
+      width: 240, height: 80,
+      'show-faces' => false,
+      stream: false, header: false,
+      'border-color' => '#ffffff'
+    }
 
     render
   end
