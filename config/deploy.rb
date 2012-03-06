5da9d7e16170b200476e464056fa1012a1638d26
Krzysztof Zalewski <zlw.zalewski@gmail.com>
2012-03-06 17:14:55 +0100

    update and clean up deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,28 +1,32 @@
 require 'bundler/capistrano'
 
-set :application, "prima-rewal"
+set :application, "prima_rewal"
 
 set :deploy_to, '/home/prima/www/prima-rewal'
-set :user, 'prima'
+set :user,      'prima'
 
-set :scm, :git
-set :scm_user, 'zlw'
-set :repository,  'git://github.com/Kangaru/prima-rewal.git'
-set :branch, 'capistrano'
+set :scm,        'git'
+set :scm_user,   'zlw'
+set :repository, 'git://github.com/Kangaru/prima-rewal.git'
+set :branch,     'capistrano'
 
 
-role :web, "prima.megiteam.pl"                # Your HTTP server, Apache/etc
-role :app, "prima.megiteam.pl"                # This may be the same as your `Web` server
-role :db,  "prima.megiteam.pl", primary: true # This is where Rails migrations will run
+role :web, 'prima.megiteam.pl'                # Your HTTP server, Apache/etc
+role :app, 'prima.megiteam.pl'                # This may be the same as your `Web` server
+role :db,  'prima.megiteam.pl', primary: true # This is where Rails migrations will run
 # role :db,  "your slave db-server here"
 
 set :use_sudo, false
 
+after 'deploy:update_code',     'deploy:assets_precompile'
+after 'deploy:setup',           'deploy:db:setup'
+after 'deploy:finalize_update', 'deploy:db:symlink'
+
 
 namespace :deploy do
-   task :start do ; end
-   task :stop do ; end
-   task :restart, :roles => :app, :except => { :no_release => true } do
+   task :start do; end
+   task :stop do; end
+   task :restart, roles: :app, except: { no_release: true } do
      run "restart-app #{application}"
    end
 end
@@ -32,27 +36,10 @@ namespace :deploy do
     run "cd #{release_path}; rm -rf public/assets/*"
     run "cd #{release_path}; RAILS_ENV=production bundle exec rake assets:precompile"
   end
-
-  after 'deploy:update_code', 'deploy:assets_precompile'
 end
 
 namespace :deploy do
   namespace :db do
-    desc <<-DESC
-      Creates the database.yml configuration file in shared path.
-
-      By default, this task uses a template unless a template \
-      called database.yml.erb is found either is :template_dir \
-      or /config/deploy folders. The default template matches \
-      the template for config/database.yml file shipped with Rails.
-
-      When this recipe is loaded, db:setup is automatically configured \
-      to be invoked after deploy:setup. You can skip this task setting \
-      the variable :skip_db_setup to true. This is especially useful \
-      if you are using this recipe in combination with \
-      capistrano-ext/multistaging to avoid multiple db:setup calls \
-      when running deploy:setup for all stages one by one.
-    DESC
     task :setup, :except => { :no_release => true } do
 
       template = <<-EOF
@@ -63,6 +50,8 @@ namespace :deploy do
         password: #{Capistrano::CLI.ui.ask("Enter #{application} database password: ")}
         encoding: unicode
         pool: 5
+        host: prima.megiteam.pl
+        port: 5435
       EOF
 
       run "mkdir -p #{shared_path}/db"
@@ -73,12 +62,8 @@ namespace :deploy do
     desc <<-DESC
       [internal] Updates the symlink for database.yml file to the just deployed release.
     DESC
-    task :symlink, :except => { :no_release => true } do
+    task :symlink, except: { no_release: true } do
       run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
     end
-
   end
-
-  after "deploy:setup",           "deploy:db:setup"   unless fetch(:skip_db_setup, false)
-  after "deploy:finalize_update", "deploy:db:symlink"
 end
