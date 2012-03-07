56fc9218c04efcc52b66bcd05de4db3f5a6dc502
Krzysztof Zalewski <zlw.zalewski@gmail.com>
2012-03-07 13:06:11 +0100

    create FlatpageSweeper

---
+++ app/sweepers/flatpage_sweeper.rb
@@ -0,0 +1,27 @@
+class FlatpageSweeper < ActionController::Caching::Sweeper
+  observe Flatpage, Flatpage::Translation
+
+  def sweep(flatpage)
+    clear_menu_cell_cache
+  end
+
+  alias_method :after_create, :sweep
+  alias_method :after_update, :sweep
+  alias_method :after_delete, :sweep
+
+
+private
+
+
+  # Destroy every cache created by MenuCell
+  # Supported keys:
+  #   - cells/menu/display
+  #   - cells/menu/display/
+  #   - cells/menu/display//
+  #   - cells/menu/display/pl
+  #   - cells/menu/display/pl/strona/10-oferta
+  #   - etc. ...
+  def clear_menu_cell_cache
+    Rails.cache.delete_matched /cells\/menu\/display\S+/
+  end
+end
