967c099e795647fd7e76d4247143464a5c8e0611
Krzysztof Zalewski <zlw.zalewski@gmail.com>
2012-03-07 04:22:38 +0100

    patch active_attr to parse dates

--- app/models/form/reservation.rb
+++ app/models/form/reservation.rb
@@ -1,6 +1,11 @@
 class Form::Reservation
   include ActiveAttr::Model
 
+  def initialize(args = {})
+    args = parse_dates(args) if args.present?
+    super args
+  end
+
   attribute :full_name
   attribute :phone_number
   attribute :email
@@ -10,11 +15,31 @@ class Form::Reservation
   attribute :notes
 
 
-  validates_presence_of :full_name, :phone_number, :email, :people
+  validates_presence_of :full_name, :phone_number, :email, :people, :date_of_arrival, :date_of_departure
 
   validates :full_name, length: { minimum: 3}, format: /\A[A-Z][a-z]+ [A-Z][a-z]+\z/i
   validates :phone_number, length: { maximum: 20 }, format: /\A\+?\d{2,3} [\d -]+\z/i
   validates :email, format: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   validates :people, inclusion: { in: 1..6 }, numericality: true
   validates :notes, length: { minimum: 3, allow_blank: true }
+
+
+private
+
+  def parse_dates(attrs)
+    attrs.merge({
+      date_of_arrival: parse_date(:date_of_arrival, attrs),
+      date_of_departure: parse_date(:date_of_departure, attrs)
+    })
+  end
+
+  def parse_date(name, attrs)
+    date_attrs = attrs.values_at("#{name}(1i)", "#{name}(2i)", "#{name}(3i)").compact
+
+    if date_attrs.present?
+      Date.parse date_attrs.join('-')
+    end
+  rescue
+    nil
+  end
 end
--- spec/models/form/reservation_spec.rb
+++ spec/models/form/reservation_spec.rb
@@ -26,4 +26,27 @@ describe Form::Reservation do
   it { should allow_value(nil).for(:notes) }
   it { should allow_value('').for(:notes) }
   it { should allow_value(' ').for(:notes) }
+
+  context "#parse_date" do
+    it 'should parse date_of_arrival properly' do
+      params = { :'date_of_arrival(1i)' => 2012, :'date_of_arrival(2i)' => 11, :'date_of_arrival(3i)' => 4 }.with_indifferent_access
+      date = Date.new(2012, 11, 4)
+
+
+      Date.should_receive(:parse).with('2012-11-4').and_return date
+
+      form = Form::Reservation.new(params)
+      form.date_of_arrival.should == date
+    end
+
+    it 'should parse date_of_departure properly' do
+      params = { :'date_of_departure(1i)' => 1992, :'date_of_departure(2i)' => 8, :'date_of_departure(3i)' => 3 }.with_indifferent_access
+      date = Date.new(1992, 8, 3)
+
+      Date.should_receive(:parse).with('1992-8-3').and_return date
+
+      form = Form::Reservation.new(params)
+      form.date_of_departure.should == date
+    end
+  end
 end
