fe41e7eefde9e04fda151d4c829d83735baef87a
Krzysztof Zalewski <zlw.zalewski@gmail.com>
2012-03-07 20:09:27 +0100

    clean up Flatpage model

--- app/models/flatpage.rb
+++ app/models/flatpage.rb
@@ -7,14 +7,6 @@ class Flatpage < ActiveRecord::Base
 
   after_initialize :setup_translations
 
-  def self.for_main_page
-    order_by_position.first
-  end
-
-  def self.for_menu
-    order_by_position.includes(:translations)
-  end
-
   def setup_translations
     if new_record? && translations.empty?
       Language.available_locales.each do |locale|
@@ -23,11 +15,41 @@ class Flatpage < ActiveRecord::Base
     end
   end
 
-  def self.update_positions(ids)
-    ids.each_with_index do |id, index|
-      update_position id, index.to_i + 1
+
+  class << self
+    def for_main_page
+      order_by_position.first
+    end
+
+    def for_menu
+      order_by_position.includes(:translations)
+    end
+    alias_method :fetch_all, :for_menu
+
+
+  private
+
+    def order_by_position
+      order('position ASC')
     end
   end
+
+  class << self
+    def update_positions(ids)
+      ids.each_with_index do |id, index|
+        update_position id, index.to_i + 1
+      end
+    end
+
+
+  private
+
+    def update_position(id, position)
+      flatpage = where('id == ? AND position != ?', id, position).limit(1).first
+      flatpage.update_attributes(position: position) if flatpage
+    end
+  end
+
 
   def to_param
     [id, title.to_url].join '-'
@@ -35,17 +57,5 @@ class Flatpage < ActiveRecord::Base
 
   def self.expiration_code(path)
     [I18n.locale, "#{path}"[1..-1].gsub(/^(I18n.available_locales * '|')/, '')]
-  end
-
-
-private
-
-  def self.order_by_position(order = 'ASC')
-    order("position #{order}")
-  end
-
-  def self.update_position(id, position)
-    flatpage = where('id == ? AND position != ?', id, position).limit(1).first
-    flatpage.update_attributes(position: position) if flatpage
   end
 end
