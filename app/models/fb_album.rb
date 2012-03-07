37b560fce2422919ae2ea28f6f98c7a4cb65859a
Krzysztof Zalewski <zlw.zalewski@gmail.com>
2012-03-07 16:29:43 +0100

    new FbAlbum model. uses cache

--- app/models/fb_album.rb
+++ app/models/fb_album.rb
@@ -1,9 +1,25 @@
 class FbAlbum
   def self.gallery
-    FbGraph::Album.fetch('177381578961541').photos
+    fetch_photos(:gallery, 177381578961541)
   end
 
   def self.carousel
-    FbGraph::Album.fetch('175604345805931').photos
+    fetch_photos(:carousel, 175604345805931)
+  end
+
+
+private
+
+  # Fetch photos from cache or from given Facebook album
+  #
+  # @param cache_name [Symbol]
+  # @param album_id   [Integer]
+  # @param separator  [String]
+  #
+  # @return [Array]
+  def self.fetch_photos(cache_name, album_id, separator = '||')
+    Rails.cache.fetch("fbalbum/#{cache_name}/photos") do
+      FbGraph::Album.fetch(album_id).photos.map(&:source)
+    end
   end
 end
--- spec/models/fb_album_spec.rb
+++ spec/models/fb_album_spec.rb
@@ -1,21 +1,39 @@
 require 'spec_helper'
 
 describe FbAlbum do
-  it 'should fetch for fbgallery' do
-    album = mock(FbGraph::Album)
+  before(:all) { ActionController::Base.perform_caching = true }
+  after(:all) { ActionController::Base.perform_caching = false }
 
-    FbGraph::Album.should_receive(:fetch).with('177381578961541').and_return album
-    album.should_receive(:photos)
+  before { ActionController::Base.cache_store.clear }
+
+
+  it 'should fetch gallery from facebook' do
+    album, photos = mock(FbGraph::Album), [mock(FbGraph::Photo), mock(FbGraph::Photo)]
+
+    FbGraph::Album.should_receive(:fetch).with(177381578961541).and_return album
+    album.should_receive(:photos).and_return photos
+    photos.should_receive(:map)
 
     FbAlbum.gallery
   end
 
-  it 'should fetch for carousel' do
-    album = mock(FbGraph::Album)
+  it 'should fetch gallery from cache' do
+    Rails.cache.should_receive(:fetch).with('fbalbum/gallery/photos').and_return ['photo1', 'photo2']
+    FbAlbum.gallery.should == ['photo1', 'photo2']
+  end
 
-    FbGraph::Album.should_receive(:fetch).with('175604345805931').and_return album
-    album.should_receive(:photos)
+  it 'should fetch carousel from facebook' do
+    album, photos = mock(FbGraph::Album), [mock(FbGraph::Photo), mock(FbGraph::Photo)]
+
+    FbGraph::Album.should_receive(:fetch).with(175604345805931).and_return album
+    album.should_receive(:photos).and_return photos
+    photos.should_receive(:map)
 
     FbAlbum.carousel
+  end
+
+  it 'should fetch carousel from cache' do
+    Rails.cache.should_receive(:fetch).with('fbalbum/carousel/photos').and_return ['photo1', 'photo2']
+    FbAlbum.carousel.should == ['photo1', 'photo2']
   end
 end
