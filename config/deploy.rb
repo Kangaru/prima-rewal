1e1d569736ecd9e02ac45a7df0100384f543177f
Krzysztof Zalewski <zlw.zalewski@gmail.com>
2012-03-07 21:00:18 +0100

    update deploy config to ask for SMTP configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,9 +19,12 @@ role :db,  'prima.megiteam.pl', primary: true # This is where Rails migrations w
 set :use_sudo, false
 
 after 'deploy:update_code',     'deploy:assets_precompile'
-after 'deploy:setup',           'deploy:db:setup'
-after 'deploy:finalize_update', 'deploy:db:symlink'
 
+after 'deploy:setup',           'deploy:db:setup'
+after 'deploy:setup',           'deploy:mailer:setup'
+
+after 'deploy:finalize_update', 'deploy:db:symlink'
+after 'deploy:finalize_update', 'deploy:mailer:symlink'
 
 namespace :deploy do
    task :start do; end
@@ -35,6 +38,31 @@ namespace :deploy do
   task :assets_precompile do
     run "cd #{release_path}; rm -rf public/assets/*"
     run "cd #{release_path}; RAILS_ENV=production bundle exec rake assets:precompile"
+  end
+end
+
+namespace :deploy do
+  namespace :mailer do
+    task :setup, except: { no_release: true } do
+
+      template = <<-EOF
+      production
+        address: 'smtp.gmail.com'
+        port: 587
+        domain: 'gmail.com'
+        user_name: #{Capistrano::CLI.ui.ask("Enter SMTP username: ")}
+        password:  #{Capistrano::CLI.ui.ask("Enter SMTP password: ")}
+        authentication: 'plain'
+        enable_starttls_auto: true
+      EOF
+
+      run "mkdir -p #{shared_path}/config"
+      put template, "#{shared_path}/config/action_mailer.yml"
+    end
+
+    task :symlink, except: { no_release: true } do
+      run "ln -nfs #{shared_path}/config/action_mailer.yml #{release_path}/config/action_mailer.yml"
+    end
   end
 end
 
