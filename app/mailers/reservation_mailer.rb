b9d9c6eca1506f5318853df54457f0d80c18e2bb
Krzysztof Zalewski <zlw.zalewski@gmail.com>
2012-03-11 00:18:22 +0100

    mailer setting

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -1,8 +1,8 @@
 class ReservationMailer < ActionMailer::Base
-  default from: "no-reply@prima-rewal.pl"
+  default from: "no-reply@prima.megiteam.pl"
 
   def book(reservation)
     @reservation = reservation
-    mail to: "zlw.zalewski@gmail.com", reply_to: @reservation.email ,subject: "Rezerwacja (#{@reservation.full_name})"
+    mail to: "zlw.zalewski@gmail.com", reply_to: @reservation.email, subject: "Rezerwacja (#{@reservation.full_name})"
   end
 end
