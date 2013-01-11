36ccdc057769be8ba068572c60df00392a8dc867
Krzysztof Zalewski <zlw.zalewski@gmail.com>
2013-01-11 15:02:18 +0100

    clean-up #reservation_form helper

--- app/helpers/forms/reservation_helper.rb
+++ app/helpers/forms/reservation_helper.rb
@@ -1,6 +1,8 @@
 module Forms::ReservationHelper
   def reservation_form(object, *args, &block)
     options = args.extract_options!
-    simple_form_for(object, *(args << options.merge(builder: Forms::Builders::Reservation)), &block)
+    args    = args << options.merge(builder: Forms::Builders::Reservation)
+
+    simple_form_for(object, *args, &block)
   end
 end
